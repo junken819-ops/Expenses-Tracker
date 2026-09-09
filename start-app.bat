c6fc0e7af7c4d035d1d2a625d7e97a77ce6830a5
junken819-ops <junken819@gmail.com>
2026-09-09 14:54:06 +0800

    Add files via upload

---
+++ start-app.bat
@@ -0,0 +1,9 @@
+@echo off
+title Pocket Winnie Server
+cd /d "%~dp0"
+echo ==========================================
+echo   Starting Pocket Winnie Server...
+echo ==========================================
+start http://localhost:3000
+node server.js
+pause
